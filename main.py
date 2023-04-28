d16706a693a4e7d30ef2907523b6dc67e440393e
ARUN NOOB <83957038+arunsingh-creator@users.noreply.github.com>
2023-04-28 19:01:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -146,6 +146,31 @@ async def account_login(bot: Client, m: Message):
         await m.reply_text(str(e))
     await m.reply_text("Done")
 
+@bot.on_message(filters.command("forward"))
+async def forward(bot: Client , m: Message):
+    msg = await bot.ask(m.chat.id, "**Forward any message from the Target channel\nBot should be admin at both the Channels**")
+    t_chat = msg.forward_from_chat.id
+    msg1 = await bot.ask(m.chat.id, "**Send Starting Message From Where you want to Start forwarding**")
+    msg2 = await bot.ask(m.chat.id, "**Send Ending Message from same chat**")
+   # print(msg1.forward_from_message_id, msg1.forward_from_chat.id, msg1.forward_from_message_id)
+    i_chat = msg1.forward_from_chat.id
+    s_msg = int(msg1.forward_from_message_id)
+    f_msg = int(msg2.forward_from_message_id)+1
+    await m.reply_text('**Forwarding Started**\n\nPress /restart to Stop and /log to get log TXT file')
+    try:
+        for i in range(s_msg, f_msg):
+            try:
+                await bot.copy_message(
+                    chat_id= t_chat,
+                    from_chat_id= i_chat,
+                    message_id= i
+                )
+                
+            except Exception:
+                continue
+    except Exception as e:
+        await m.reply_text(str(e))
+    await m.reply_text("Done Forwarding")
 
 
 
